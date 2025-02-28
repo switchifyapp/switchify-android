aad8732cf378b5eefe77b4f598bb89ff1c55fb62
OwenMcGirr <o.a.mcgirr@gmail.com>
2025-02-28 19:36:28 +0000

    Implement a separate scanning process for keyboard
    closes #992

---
+++ app/src/main/java/com/enaboapps/switchify/service/methods/nodes/KeyboardScanner.kt
@@ -0,0 +1,51 @@
+package com.enaboapps.switchify.service.methods.nodes
+
+import android.content.Context
+import android.util.Log
+import com.enaboapps.switchify.service.scanning.tree.ScanTree
+
+class KeyboardScanner {
+    private lateinit var scanTree: ScanTree
+
+    companion object {
+        private const val TAG = "KeyboardScanner"
+    }
+
+    /**
+     * Starts the keyboard scanner.
+     *
+     * @param context The context in which the keyboard scanner is started.
+     */
+    fun start(context: Context) {
+        scanTree = ScanTree(
+            context = context,
+            stopScanningOnSelect = true
+        )
+    }
+
+    /**
+     * Updates the keyboard scanner with the given nodes.
+     *
+     * @param nodes The list of nodes to update the keyboard scanner with.
+     */
+    fun updateNodes(nodes: List<Node>) {
+        Log.d(TAG, "Updating keyboard scanner with ${nodes.size} nodes")
+        scanTree.buildTree(nodes)
+    }
+
+    /**
+     * Gets the scan tree for the keyboard scanner.
+     *
+     * @return The scan tree for the keyboard scanner.
+     */
+    fun getScanTree(): ScanTree {
+        return scanTree
+    }
+
+    /**
+     * Cleans up the keyboard scanner.
+     */
+    fun cleanup() {
+        scanTree.cleanup()
+    }
+}
