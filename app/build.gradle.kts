68f327cb359378de6a94d680cb4192a75247e258
OwenMcGirr <o.a.mcgirr@gmail.com>
2025-05-16 16:21:34 +0100

    Bump version name to 1.77.9

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 31
         targetSdk = 35
         versionCode = gitVersionCode()
-        versionName = "1.77.8"
+        versionName = "1.77.9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
