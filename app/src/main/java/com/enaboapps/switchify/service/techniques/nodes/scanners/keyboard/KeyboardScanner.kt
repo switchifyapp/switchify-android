9508bb2ef96873bfa58dcb53eba167457664ff7c
OwenMcGirr <o.a.mcgirr@gmail.com>
2025-05-16 09:36:25 +0100

    Implement duplicate update detection in BaseNodeScanner
    closes #1097

--- app/src/main/java/com/enaboapps/switchify/service/techniques/nodes/scanners/keyboard/KeyboardScanner.kt
+++ app/src/main/java/com/enaboapps/switchify/service/techniques/nodes/scanners/keyboard/KeyboardScanner.kt
@@ -5,23 +5,17 @@ import com.enaboapps.switchify.service.techniques.nodes.Node
 import com.enaboapps.switchify.service.techniques.nodes.scanners.BaseNodeScanner
 
 class KeyboardScanner : BaseNodeScanner() {
-    private val currentNodes = mutableListOf<Node>()
-
     companion object {
         private const val TAG = "KeyboardScanner"
     }
 
     /**
      * Updates the keyboard scanner with the given nodes.
+     * Duplicate detection is handled by the parent class.
      *
      * @param nodes The list of nodes to update the keyboard scanner with.
      */
     override fun updateNodes(nodes: List<Node>) {
-        if (currentNodes == nodes) {
-            return
-        }
-        currentNodes.clear()
-        currentNodes.addAll(nodes)
         Log.d(TAG, "Updating keyboard scanner with ${nodes.size} nodes")
         super.updateNodes(nodes)
     }
