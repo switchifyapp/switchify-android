e64dd9d2b0b51c1186e68f1fa8ede71a9fe6b378
OwenMcGirr <o.a.mcgirr@gmail.com>
2025-05-30 10:26:20 +0100

    chore: Bump version to 1.82.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,7 +21,7 @@ android {
         minSdk = 31
         targetSdk = 36
         versionCode = gitVersionCode()
-        versionName = "1.82.0"
+        versionName = "1.82.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
