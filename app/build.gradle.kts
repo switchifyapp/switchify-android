6aa60a53526c1b609eefedb2c25f6182170ff862
OwenMcGirr <o.a.mcgirr@gmail.com>
2025-07-22 19:43:22 +0100

    chore: bump version to 1.95.0-beta.5

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -27,7 +27,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = gitVersionCode()
-        versionName = "1.95.0-beta.4"
+        versionName = "1.95.0-beta.5"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
