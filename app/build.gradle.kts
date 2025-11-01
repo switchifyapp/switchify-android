0c043be5d94d487b998eea5d20e91ed77881c2ce
OwenMcGirr <3359031+OwenMcGirr@users.noreply.github.com>
2025-11-01 11:27:31 +0000

    chore: bump version to 2.7.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = gitVersionCode()
-        versionName = "2.6.4"
+        versionName = "2.7.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
