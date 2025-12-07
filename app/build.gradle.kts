129e522874e7188232f3807ab518fc1f6efed295
OwenMcGirr <3359031+OwenMcGirr@users.noreply.github.com>
2025-12-07 17:21:09 +0000

    chore: bump version to 2.10.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -28,7 +28,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = gitVersionCode()
-        versionName = "2.10.0"
+        versionName = "2.10.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
