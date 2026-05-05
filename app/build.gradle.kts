f0a265c60c9fe825255abe5a5d3c0d9107de086e
Owen McGirr <o.a.mcgirr@gmail.com>
2026-05-05 17:18:56 +0100

    Release version 2.19.0
    - Bump app versionName to 2.19.0
    - Prepare v2.19.0 release milestone for publication
    
    Auto-generated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = gitVersionCode()
-        versionName = "2.18.1"
+        versionName = "2.19.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
