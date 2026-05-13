1e522fb3264a0710e2da09a0474697980d773adb
Owen McGirr <o.a.mcgirr@gmail.com>
2026-05-13 14:20:42 +0100

    chore: release version 2.24.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = gitVersionCode()
-        versionName = "2.24.0"
+        versionName = "2.24.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
