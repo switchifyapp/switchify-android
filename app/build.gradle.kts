37ae130cf6f3a9fda3da6fab4336bc7e298e2187
Owen McGirr <o.a.mcgirr@gmail.com>
2026-05-24 21:15:46 +0100

    Bump version to 2.28.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = gitVersionCode()
-        versionName = "2.28.0"
+        versionName = "2.28.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
