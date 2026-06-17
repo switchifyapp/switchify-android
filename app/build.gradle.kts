021000c4ee487bd9b13f1969cf5a919592cf5d4c
Owen McGirr <o.a.mcgirr@gmail.com>
2026-06-17 20:11:04 +0100

    Bump version to 2.31.0-beta.40
    - Update release versionName for beta 40
    
    - Prepare direct boot startup fix release
    
    Auto-generated

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,7 +20,7 @@ android {
         minSdk = 29
         targetSdk = 36
         versionCode = gitVersionCode()
-        versionName = "2.31.0-beta.39"
+        versionName = "2.31.0-beta.40"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
